c3157420303cd5af6e8920673844f7bf3ca0d100
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-14 08:53:31 -0300

    Rand Message: /tangible-ant

--- src/bin/01_weird_algorithm.rs
+++ src/bin/01_weird_algorithm.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn main() {
+    let mut n: u32 = std::io::read_to_string(std::io::stdin())
+        .unwrap()
+        .trim()
+        .parse()
+        .unwrap();
+
+    print!("{n}");
+
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        print!("{}", n);
+    }
+}
