66deab2d9f2d04e73a8edd1e7d2fe249365f337c
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-14 13:49:53 -0300

    Rand Message: /nine-pleasure

--- src/bin/02_missing_number.rs
+++ src/bin/02_missing_number.rs
@@ -1,2 +1,15 @@
-fn main() {:q
+fn main() {
+    let mut l = std::io::stdin().lines().take(2);
+    let (f, s) = (l.next().unwrap(), l.next().unwrap());
+
+    let n: u64 = f.unwrap().parse().unwrap();
+    let sum: u64 = s
+        .unwrap()
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .sum();
+
+    let res = n * (n + 1) / 2 - sum;
+
+    println!("{res}")
 }
